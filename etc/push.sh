f7274df827a5b0833604ca3103016ef9031a27f0
Graham Moucka <mouckatron@gmail.com>
2020-05-09 12:56:07 +0100

    Fixed syntax errors and added __init__.py to push

--- etc/push.sh
+++ etc/push.sh
@@ -4,7 +4,7 @@ WHEREIAM="${BASH_SOURCE%/*}/"
 
 ampy -p /dev/ttyUSB0 -b 115200 mkdir etc 2>/dev/null
 
-for file in $(find $WHEREIAM -type f \( -name "*.json" ! -name "*.example.json" \))
+for file in $(find $WHEREIAM -type f \( -name "*.json" ! -name "*.example.json" \) -or -name "__init__.py")
 do
     echo $file
     ampy -p /dev/ttyUSB0 -b 115200 put $file /etc/$(basename $file)
